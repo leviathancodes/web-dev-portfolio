41c68149c7f5d566989e97db61f45965973bff12
lhuddlesto <lance299@gmail.com>
2019-11-01 12:22:04 -0400

    Added meta tags with Helmet

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,20 @@
 import React from "react"
+import { Helmet } from "react-helmet";
 import ContactForm from "../components/ContactForm"
 
 const Contact = () => {
     return (
         <div className="contact">
+            <Helmet>
+                <title>Contact A Web Developer - Let's build your website - Lance Huddleston II</title>
+                <meta name="author" content="Lance Huddleston II" />
+                <meta name="description" content="Get in contact with a web developer today. Lance Huddleston II can help you build web sites and web applications for your business.
+                With his experience, the job will get done and it'll be done well."/>
+                <meta property="og:title" content="Contact A Web Developer - Let's build your website - Lance Huddleston II" />
+                <meta property="og:description" content="Get in contact with a web developer today. Lance Huddleston II can help you build web sites and web applications for your business.
+                With his experience, the job will get done and it'll be done well." />
+                <meta name="twitter:title" content="Contact A Web Developer - Let's build your website - Lance Huddleston II"/>
+            </Helmet>
             <div className="contact__grid">
                 <div className="contactIntro">
                 <h1 className="contactIntro__head">We’ll do great work.</h1>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from "react"
 import Landing from '../components/Landing'
 import About from '../components/About'
+import { Helmet } from "react-helmet";
 
 const Home = () => {
     return (
         <div className="wrapper"> 
+            <Helmet>
+                <title>Web Developer - Front-end/Back-end Services | Lance Huddleston II</title>
+                <meta name="author" content="Lance Huddleston II" />
+                <meta name="description" content="Get in contact with a web developer today. Lance Huddleston II can help you build web sites and web applications for your business.
+                With his experience, the job will get done and it'll be done well."/>
+                <meta property="og:title" content="Web Developer - Front-end/Back-end Services | Lance Huddleston II" />
+                <meta property="og:description" content="Get in contact with a web developer today. Lance Huddleston II can help you build web sites and web applications for your business.
+                With his experience, the job will get done and it'll be done well." />
+                <meta name="twitter:title" content="Web Developer - Front-end/Back-end Services | Lance Huddleston II"/>
+            </Helmet>
+
             <Landing />
             <About />
       </div>
